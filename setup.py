43012f134420d6e2f5641f97726ea78e497a90e5
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-10-30 11:24:08 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pylightrequests',
+    version='0.0.1',
+    packages=['pylightrequests'],
+    url='https://github.com/pneb/pylightrequests',
+    license='MIT',
+    author='pneb',
+    author_email='contact@bern.codes',
+    description='Fast and simple python http client, uses sockets'
+)
