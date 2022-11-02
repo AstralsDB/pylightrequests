1e882a589b5658fb1b3a177ba36ff8259789bc90
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-11-02 15:26:13 +0800

    Update pylightrequests.py

--- pylightrequests/pylightrequests.py
+++ pylightrequests/pylightrequests.py
@@ -1,19 +1,10 @@
 import ssl
 import socket
-import multiprocessing
 import re
-from .error import *
+import multiprocessing
+from error import *
 
 class HTTP:
-
-    def __init__(self):
-        self.code = None
-        self.header = None
-        self.data = None
-        self.json = None
-        self.status = None
-        self.json_data = None
-
     def get_req(self, host, path, port=80, ssl=False):
         if ssl:
             socket_ = ssl.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
@@ -84,7 +75,6 @@ class HTTP:
             socket_.close()
         if not data:
             raise RequestError(data)
-        self._parse_data(data)
         return data
 
 
@@ -96,41 +86,3 @@ class HTTP:
         p.close()
         p.join()
         return results
-
-
-    def _parse_data(self, data):
-        self.code = re.findall('HTTP/1.0 \d\d\d .+\r\n', data)[0].split(' ')[1]
-        self.json_data = re.findall('\{.+\}', data)
-        self.json = self.json_data[0] if self.json_data else None
-        self.data = re.findall('\r\n\r\n.+\r\n', data)[0] if re.findall('\r\n\r\n.+\r\n', data) else None
-        self.header = re.findall('HTTP/1.0 \d\d\d .+\r\n.+\r\n', data)[0] if re.findall('HTTP/1.0 \d\d\d .+\r\n.+\r\n', data) else None
-        self.status = self.code if self.code else None
-
-
-    def parse_data(self, data):
-        self._parse_data(data)
-        return self.__dict__
-
-
-    def get_response_code(self):
-        return self.code
-
-
-    def get_status(self):
-        return self.status
-
-
-    def get_header(self):
-        return self.header
-
-
-    def get_json(self):
-        return self.json
-
-
-    def get_json_data(self):
-        return self.json_data
-
-
-    def get_data(self):
-        return self.data
